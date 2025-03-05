4d616b18ec743b6e71129b960b04992e9651f734
Aman Fangeria <amanfangeria980@gmail.com>
2025-03-05 20:28:53 +0530

    feat: app completed

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    res.send("Hello World");
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
 });
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
